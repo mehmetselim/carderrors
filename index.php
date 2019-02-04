a3e6d700cbff9025cef1f9ee16b229b3d673fc7b
Mehmet Selim Dil <mehmetselimdil@gmail.com>
2019-02-04 16:13:36 +0300

    add create table

--- index.php
+++ index.php
@@ -3,18 +3,28 @@
 
 $db = parse_url(getenv("DATABASE_URL"));
 
-$pdo = new PDO("pgsql:" . sprintf(
+
+
+try {
+    $conn = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
     $db["host"],
     $db["port"],
     $db["user"],
     $db["pass"],
     ltrim($db["path"], "/")
-));
+	));
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 
-var_dump($pdo);
-exit;
 
+/*
 $sql_create_errors_tbl = "CREATE TABLE errors (
     id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     error VARCHAR(255) NOT NULL,
@@ -25,3 +35,4 @@ $result = $pdo->exec($sql_create_errors_tbl);
 
 var_dump($result);
 exit;
+*/
