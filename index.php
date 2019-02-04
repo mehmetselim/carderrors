388b7adab2ca0f830e37600a5cc42844f35b0232
Mehmet Selim Dil <mehmetselimdil@gmail.com>
2019-02-04 16:14:52 +0300

    add create table

--- index.php
+++ index.php
@@ -16,7 +16,17 @@ try {
 	));
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    
+    $sql_create_errors_tbl = "CREATE TABLE errors (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    error VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
+    )"
+
+$result = $conn->exec($sql_create_errors_tbl);
+
+var_dump($result);
+exit;
     }
 catch(PDOException $e)
     {
@@ -25,14 +35,5 @@ catch(PDOException $e)
 
 
 /*
-$sql_create_errors_tbl = "CREATE TABLE errors (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    error VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL
-    )"
-
-$result = $pdo->exec($sql_create_errors_tbl);
-
-var_dump($result);
 exit;
 */
